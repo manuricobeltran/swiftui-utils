4dd86057832e5a4048c00c8fa244f99cae694170
Manu Rico <ricobeltranmanuel@gmail.com>
2022-12-30 07:06:12 +0100

    Initial Commit

---
+++ SwiftUI Utils/SwiftUI_UtilsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_UtilsApp.swift
+//  SwiftUI Utils
+//
+//  Created by Manu Rico on 30/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_UtilsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
